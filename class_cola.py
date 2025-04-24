d508f6a321c11a09304b618ff629dbdcba6aacdb
Hugo Mejía <165737217+hamejia1@users.noreply.github.com>
2025-04-23 20:14:58 -0400

    Add files via upload
    Class Nodo, Class Cola, def esta_vacia

---
+++ class_cola.py
@@ -0,0 +1,13 @@
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+
+class Cola:
+    def __init__(self):
+        self.frente = None
+        self.final = None
+
+    def esta_vacia(self):
+        return self.frente is None
+
